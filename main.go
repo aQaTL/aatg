6acbbd1d7722b9eee2b1a82c13fd98f47decfe48
aQaTL <mmsoltys@outlook.com>
2017-07-22 11:04:25 +0200

    New line handling and output buffering

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"flag"
 	"os"
 	"bufio"
-	"fmt"
+	"strings"
 )
 
 var (
-	text = flag.String("t", "Hello, World!", "Specify custom text")
+	text       = flag.String("t", "Hello, World!", "Specify custom text")
 	pipedInput = flag.Bool("p", false, "Read from stdin instead")
 )
 
@@ -27,31 +27,33 @@ func main() {
 	if *pipedInput {
 		in := bufio.NewReader(os.Stdin)
 		text, _ := in.ReadString(0x3)
-		fmt.Println(text)
 		generateASCIIArt(os.Stdout, text)
-
 	} else {
 		generateASCIIArt(os.Stdout, *text)
 	}
 }
 
 func generateASCIIArt(w io.Writer, text string) {
-	for row := 0; row < glyphH; row++ {
-		for _, r := range text {
-			if int(r) > 0x7f {
-				r = 0x7f
-			}
-			glyphIndex := int(r) - 0x20
-			for col := 0; col < glyphW; col++ {
-				dot := glyphs[glyphW*glyphH*glyphIndex+(row*glyphW+col)]
-				if dot == 0x00 {
-					w.Write([]byte(" "))
-				} else if dot == 0xff {
-					w.Write([]byte("\u2588"))
+	buffer := bufio.NewWriter(w)
+	for _, text := range strings.Split(text, "\n") {
+		for row := 0; row < glyphH; row++ {
+			for _, r := range text {
+				if int(r) > 0x7f || int(r) < 0x20 {
+					r = 0x7f
 				}
+				glyphIndex := int(r) - 0x20
+				for col := 0; col < glyphW; col++ {
+					dot := glyphs[glyphW*glyphH*glyphIndex+(row*glyphW+col)]
+					if dot == 0x00 {
+						buffer.WriteRune(' ')
+					} else if dot == 0xff {
+						buffer.WriteRune('\u2588')
+					}
+				}
+				buffer.WriteRune(' ')
 			}
-			w.Write([]byte(" "))
+			buffer.WriteRune('\n')
 		}
-		w.Write([]byte("\n"))
 	}
+	buffer.Flush()
 }
